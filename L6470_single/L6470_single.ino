0906a9a4a4ee4f41f4f20995258adf834cf565a0
ganezasan <istaka12@gmail.com>
2017-12-03 19:11:34 +0900

    fix comment

--- L6470_single/L6470_single.ino
+++ L6470_single/L6470_single.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
-#include <SPI.h>  //(1)SPI通信をするための読み込み
+#include <SPI.h>  // SPI通信をするための読み込み
 
-//(2)ステッピングモーター用のピンの定義
+// ステッピングモーター用のピンの定義
 #define PIN_SPI_MOSI 11
 #define PIN_SPI_MISO 12
 #define PIN_SPI_SCK 13
@@ -13,7 +13,7 @@ void setup()
   delay(1000);
   Serial.begin(9600);
 
-  //(3)ステッピングモーター用のピンの準備
+  // ステッピングモーター用のピンの準備
   pinMode(PIN_SPI_MOSI, OUTPUT);
   pinMode(PIN_SPI_MISO, INPUT);
   pinMode(PIN_SPI_SCK, OUTPUT);
@@ -21,17 +21,17 @@ void setup()
   digitalWrite(PIN_SPI_SS, HIGH);
   pinMode(PIN_BUSY, INPUT);
 
-  //(4)SPI通信の開始宣言
+  // SPI通信の開始宣言
   SPI.begin();
   SPI.setDataMode(SPI_MODE3);//SCKの立上りでテータを送受信、アイドル時はpinをHIGHに設定
   SPI.setBitOrder(MSBFIRST);//MSBから送信
 
-  //(5)L6470の利用設定
-  L6470_setup(PIN_SPI_SS);
+  // L6470の利用設定
+  L6470_setup();
 }
 
 //**********************************************
-//(6)SPI通信するための関数
+// SPI通信するための関数
 //**********************************************
 void L6470_send(unsigned char value){
   while(!digitalRead(PIN_BUSY)){} //BESYが解除されるまで待機
@@ -43,7 +43,7 @@ void L6470_send(unsigned char value){
 }
 
 //**********************************************
-// (7)L6470のセットアップ
+// L6470のセットアップ
 //**********************************************
 void L6470_setup(){
   //デバイス設定
@@ -92,15 +92,15 @@ void L6470_setup(){
 }
 
 //**********************************************
-// (8)メイン処理 - loop()
+// メイン処理 - loop()
 //**********************************************
 void loop(){
   //360度 - １回転させる処理
-  L6470_send(0x51, PIN_SPI_SS);//Run(DIR,SPD),0x51:正転,0x50:逆転
-  L6470_send(0x00, PIN_SPI_SS);
-  L6470_send(0x20, PIN_SPI_SS);//回転スピードの設定
-  L6470_send(0x00, PIN_SPI_SS);
+  L6470_send(0x51);//Run(DIR,SPD),0x51:正転,0x50:逆転
+  L6470_send(0x00);
+  L6470_send(0x20);//回転スピードの設定
+  L6470_send(0x00);
 
   delay(1604);//1604msで約1回転
-  L6470_send(0xB8, PIN_SPI_SS);
+  L6470_send(0xB8);
 }
